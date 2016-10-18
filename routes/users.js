472e77907b5e1c73ae2a49a45112d91bb9b92a28
egin10 <egin066@gmail.com>
2016-10-18 18:23:01 +0800

    connecting database

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+//connecting to database (mariadb)
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'apasih10',
+  database : 'nodejs'
+});
+connection.connect();
+connection.query('USE nodejs', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('database connected');
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  connection.query('SELECT * FROM contacts', function(err, rows, fields){
+    res.json(rows);
+  });
 });
 
 module.exports = router;

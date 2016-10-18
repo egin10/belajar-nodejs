b298211298893d0daa72ed29c3ab043406ba4031
sm-alfariz <samman.alfariz@gmail.com>
2016-10-19 01:52:49 +0800

    fix conlict merge

--- app.js
+++ app.js
@@ -20,11 +20,9 @@ app.use(
       port     : 3306,
     },'request')
 );
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));

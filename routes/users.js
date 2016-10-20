b6571509be2e7d4d820cf80a1115b72c0446cba2
egin10 <egin066@gmail.com>
2016-10-20 17:57:50 +0800

    edit perameter

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,10 @@ router.get('/', function(req, res, connection) {
 router.get('/:userID', function(req, res, connection) {
   req.getConnection(function(err,connection){
      connection.query('SELECT * FROM contacts WHERE id = "' + req.params.userID + '"' ,function(err,rows) {
-        if(err)
-           console.log("Error Selecting : %s ",err );
-           res.json(rows);
+        // if(err)
+          console.log(rows[0].first_name);
+          res.json(rows[0].first_name);
+          // res.render('user', { title: 'Data User', name: nama });
          });
     });
 });
